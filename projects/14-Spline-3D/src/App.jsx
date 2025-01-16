fa06646ad3fd45f65f183ae6e748bd5478e82b47
Luis Lezcano <luislezcano.04.05@gmail.com>
2025-01-16 14:11:32 -0500

    3D Room con Spline

---
+++ projects/14-Spline-3D/src/App.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/5wKfmtLKpBRNtJcp/scene.splinecode" />
+    </main>
+  );
+}
